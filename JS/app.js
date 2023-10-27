526adf7b2ec439a64a094f46d78f2b493c0c2e20
“KauaAdrien” <kauaadrien76@gmail.com>
2023-10-26 22:04:13 -0400

    Manipulando o cardapio no js

--- JS/app.js
+++ JS/app.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
 
 var cardapio = {};
 
+var MEU_CARRINHO = [];
+
 cardapio.eventos = {
 
     init: () => {
@@ -28,7 +30,8 @@ cardapio.metodos = {
 
             let temp = cardapio.templates.item.replace(/\${img}/g, e.img)
                 .replace(/\${name}/g, e.name)
-                .replace(/\${price}/g, e.price.toFixed(2).replace('.', ','));
+                .replace(/\${price}/g, e.price.toFixed(2).replace('.', ','))
+                .replace(/\${id}/g, e.id)
 
             if (vermais && i >= 8 && i < 12) {
                 $("#itensCardapio").append(temp)
@@ -54,6 +57,57 @@ cardapio.metodos = {
         cardapio.metodos.obterItensCardapio(ativo, true)
 
         $("#btnVerMais").addClass('hidden');
+
+    },
+
+    diminuirQuantidade: (id) => {
+
+        let qntdAtual = parseInt ($("#qntd-" + id).text());
+
+        if (qntdAtual > 0) {
+            $("#qntd-" + id).text(qntdAtual - 1)
+        }
+
+    },
+
+    aumentarQuantidade: (id) => {
+
+        let qntdAtual = parseInt ($("#qntd-" + id).text());
+        $("#qntd-" + id).text(qntdAtual + 1);
+
+    },
+
+    adicionarAoCarrinho: (id) => {
+
+        let qntdAtual = parseInt ($("#qntd-" + id).text());
+
+        if (qntdAtual > 0) {
+
+            var categoria = $(".container-menu a.active").attr('id').split('menu-')[1];
+
+            let filtro = MENU[categoria];
+
+            let item = $.grep(filtro, (e, i) => { return e.id == id });
+
+            if (item.length > 0) {
+
+                let existe = $.grep(MEU_CARRINHO, (elem, index) => { return elem.id == id });
+
+                if (existe.length > 0) {
+                    let objIndex = MEU_CARRINHO.findIndex((obj => obj.id == id));
+                    MEU_CARRINHO[objIndex].qntd = MEU_CARRINHO[objIndex].qntd + qntdAtual;
+                }
+                else {
+
+                    item[0].qntd = qntdAtual;
+                    MEU_CARRINHO.push(item[0])
+                }
+
+                $("#qntd-" + id).text(0);
+
+            }
+
+        }
 
     }
 
